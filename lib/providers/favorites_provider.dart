68de8f3e86174091e6a9781ff3a4940ab9643961
Sytabram <bryan.zweiacker@sytabram.ch>
2026-06-08 14:25:41 +0200

    feat: add storage persistence to FavoritesProvider

--- lib/providers/favorites_provider.dart
+++ lib/providers/favorites_provider.dart
@@ -1,9 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:mealmate/services/storage_service.dart';
 import '../models/meal.dart';
 
 class FavoritesProvider extends ChangeNotifier {
   final List<Meal> _favorites = <Meal>[];
   List<Meal> get meals => _favorites.toList();
+
+  final StorageService _storageService = StorageService();
+  bool _isLoading = true;
+  bool get isLoading => _isLoading;
+  FavoritesProvider() {
+    _loadFromStorage();
+  }
+
+  Future<void> _loadFromStorage() async {
+    try {
+      final favorites = await _storageService.loadFavorites();
+      _favorites.addAll(favorites);
+      _isLoading = false;
+      notifyListeners();
+    } catch (e) {
+      _isLoading = false;
+      notifyListeners();
+    }
+  }
+
 
   void toggle(Meal meal) {
     if (_favorites.contains(meal)) {
@@ -11,6 +32,7 @@ class FavoritesProvider extends ChangeNotifier {
     } else {
       _favorites.add(meal);
     }
+    _storageService.saveFavorites(_favorites);
     notifyListeners();
   }
 }
